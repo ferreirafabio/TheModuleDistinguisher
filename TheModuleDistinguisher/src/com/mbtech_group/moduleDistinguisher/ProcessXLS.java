b1042e10d4062244d83538d3a50f2b6091a7429f
FERRFAB <FERRFAB@CMTCLT30711HYB.rd.corpintra.net>
2014-03-19 17:32:25 +0100

    checkstyle tested

--- TheModuleDistinguisher/src/com/mbtech_group/moduleDistinguisher/ProcessXLS.java
+++ TheModuleDistinguisher/src/com/mbtech_group/moduleDistinguisher/ProcessXLS.java
@@ -25,7 +25,6 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
  */
 
 public class ProcessXLS {
-
 	private static final int MY_MINIMUM_COLUMN_COUNT = 0;
 	private InputStream inp;
 	private OutputStream out;
